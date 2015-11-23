673caf5e8038af8f272ff0572bab11c7a97761df
Annalia Sunderland <annaliasunderland@gmail.com>
2015-11-23 16:39:11 -0500

    Initial Commit

---
+++ SliderButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SliderButton
+//
+//  Created by Annalia Sunderland on 11/23/15.
+//  Copyright © 2015 Annalia Sunderland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
